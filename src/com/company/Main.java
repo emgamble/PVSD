721ff6f70e8ef27f289a308711749956f5b0f1d7
eg913 <eg913@pvsd.org>
2018-10-05 11:13:20 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Student student1 = new Student(4.57, "Bennett");
+        Student student2 = new Student(4.35, "Tanner");
+        Student student3 = new Student(4.75, "Emma");
+        Student student4 = new Student(4.86, "Bryce");
+
+
+
+    }
+}
---
+++ src/com/company/Student.java
@@ -0,0 +1,20 @@
+package com.company;
+
+/**
+ * Created by eg913 on 10/5/18.
+ */
+public class Student {
+
+    public String name;
+    public double gpa;
+
+    public Student(double g, String n){
+            name = n;
+            gpa = g;
+
+    }
+
+
+
+    }
+}
